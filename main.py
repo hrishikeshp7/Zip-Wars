e5fe3d1f338d75a0372c03b418c7ac43b2ef77d1
Restyled.io <commits@restyled.io>
2021-06-26 07:40:22 +0000

    Restyled by autopep8

--- main.py
+++ main.py
@@ -1,14 +1,16 @@
 import os
 import zipfile
-    
+
+
 def zipdir(path, ziph):
     # ziph is zipfile handle
     for root, dirs, files in os.walk(path):
         for file in files:
-            ziph.write(os.path.join(root, file), 
-                       os.path.relpath(os.path.join(root, file), 
+            ziph.write(os.path.join(root, file),
+                       os.path.relpath(os.path.join(root, file),
                                        os.path.join(path, '..')))
-      
+
+
 zipf = zipfile.ZipFile('Python.zip', 'w', zipfile.ZIP_DEFLATED)
 zipdir('tmp/', zipf)
 zipf.close()
