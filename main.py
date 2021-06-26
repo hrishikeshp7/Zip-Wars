18b8e06b4e3e62ddbb783f56fd07b62bdc2d9d0c
Restyled.io <commits@restyled.io>
2021-06-26 07:40:28 +0000

    Restyled by black

--- main.py
+++ main.py
@@ -6,11 +6,12 @@ def zipdir(path, ziph):
     # ziph is zipfile handle
     for root, dirs, files in os.walk(path):
         for file in files:
-            ziph.write(os.path.join(root, file),
-                       os.path.relpath(os.path.join(root, file),
-                                       os.path.join(path, '..')))
+            ziph.write(
+                os.path.join(root, file),
+                os.path.relpath(os.path.join(root, file), os.path.join(path, "..")),
+            )
 
 
-zipf = zipfile.ZipFile('Python.zip', 'w', zipfile.ZIP_DEFLATED)
-zipdir('tmp/', zipf)
+zipf = zipfile.ZipFile("Python.zip", "w", zipfile.ZIP_DEFLATED)
+zipdir("tmp/", zipf)
 zipf.close()
